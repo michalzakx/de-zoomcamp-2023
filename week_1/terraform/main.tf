e5ee2de70f35524386dda15cd7187d1031c7bbc2
Michal Zak <michal.zak94@gmail.com>
2023-01-25 15:44:46 +0100

    Add Terraform configuration

---
+++ week_1/terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+  }
+}
+
+# Configure the GCP Provider
+provider "google" {
+  project     = var.project
+  region      = var.region
+  credentials = var.credentials
+}
+
+# Data Lake Bucket
+resource "google_storage_bucket" "data-lake" {
+  name     = "${local.data_lake_bucket}_${var.project}"
+  location = var.region
+
+  storage_class               = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+
+  force_destroy = true
+}
+
+# Data Warehouse
+resource "google_bigquery_dataset" "bq_taxi" {
+  dataset_id  = var.TAXI_DATA
+  project     = var.project
+  location    = var.region
+  description = "BigQuery dataset that NYC TLC Trip data will be ingested into."
+}
---
+++ week_1/terraform/variables.tf
@@ -0,0 +1,33 @@
+locals {
+  data_lake_bucket = "data_lake"
+}
+
+# Project information
+variable "project" {
+  description = "Your GCP project ID."
+  type        = string
+}
+
+variable "region" {
+  description = "Region for deploying GCP resources. Reference: https://cloud.google.com/about/locations#europe"
+  type        = string
+  default     = "europe-central2"
+}
+
+variable "credentials" {
+  description = "Path to the service account key file."
+  type        = string
+}
+
+# Data Lake Settings
+variable "storage_class" {
+  description = "Storage class type of the bucket."
+  type        = string
+  default     = "STANDARD"
+}
+
+# Data Warehouse Settings
+variable "TAXI_DATA" {
+  description = "Name of the NYC TLC Trip dataset."
+  type        = string
+}
